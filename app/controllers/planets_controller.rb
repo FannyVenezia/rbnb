4de4bc3558ba9c1f4d2bbf9b148a5d1324d20d95
Fanny <venezia.fanny@gmail.com>
2022-11-21 17:06:07 +0100

    Corrections erreurs controller planets

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,5 +1,5 @@
 class PlanetsController < ApplicationController
-  before_action :set_planet, only: %i[show new create destroy]
+  before_action :set_planet, only: %i[show edit update create destroy]
 
   def index
     @planets = Planet.all
@@ -13,8 +13,10 @@ class PlanetsController < ApplicationController
   end
 
   def create
+    @planet = Planet.new(planet_params)
+
     if @planet.save
-      redirect_to planet_path(@planet)
+      redirect_to planet_path(@planet), notice: "Planet was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
@@ -22,16 +24,27 @@ class PlanetsController < ApplicationController
 
   def destroy
     @planet.destroy
-    redirect_to planet_path(@planet), status: :see_other
+    redirect_to root_path, notice: "Planet was successfully destroyed."
+  end
+
+  def edit
+  end
+
+  def update
+    if @planet.update(planet_params)
+      redirect_to planet_path(@planet)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_planet
-    @planet = planet.find(params[:restaurant_id])
+    @planet = planet.find(params[:id])
   end
 
-  def review_params
-    params.require(:planet).permit(:name)
+  def planet_params
+    params.require(:planet).permit(:name, :mass, :area, :solar_system)
   end
 end
