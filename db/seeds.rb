af72bf04d85b1326672af3e0a91cda61246892c9
asma chouar <asmachouar@hotmail.fr>
2022-11-22 17:50:22 +0100

    indexpage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+Planet.destroy_all
 User.destroy_all
-User.create(email: 'toto@gmail.com', password: 'tototo')
+
+user_1 = User.create(email: 'toto@gmail.com', password: 'tototo')
+user_2 = User.create(email: 'tata@gmail.com', password: 'tatata')
+user_3 = User.create(email: 'titi@gmail.com', password: 'tititi')
+
+Planet.create(name: "saturne", mass: "29999999", area: "68556674745", solar_system: "solar", user: user_1)
+Planet.create(name: "uranus", mass: "29999999", area: "68556674745", solar_system: "solar", user: user_2)
+Planet.create(name: "neptune", mass: "29999999", area: "68556674745", solar_system: "solar", user: user_3)
+# Planet.create(name: "mars", mass: "29999999", area: "68556674745", solar_system: "solar", user: user_1)
+# Planet.create(name: "pluton", mass: "29999999", area: "68556674745", solar_system: "solar", user: user_1)
