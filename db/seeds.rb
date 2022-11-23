73774fadbb702ecc95432c438e4d13fea4cc22ea
Fanny <venezia.fanny@gmail.com>
2022-11-23 16:44:35 +0100

    recap final

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,6 @@ user_1 = User.create(email: 'toto@gmail.com', password: 'tototo')
 user_2 = User.create(email: 'tata@gmail.com', password: 'tatata')
 user_3 = User.create(email: 'titi@gmail.com', password: 'tititi')
 
-# Planet.create(name: "saturne", mass: "29999999", area: "68556674745", solar_system: "solar", user: user_1)
-# Planet.create(name: "uranus", mass: "29999999", area: "68556674745", solar_system: "solar", user: user_2)
-# Planet.create(name: "neptune", mass: "29999999", area: "68556674745", solar_system: "solar", user: user_3)
-# Planet.create(name: "mars", mass: "29999999", area: "68556674745", solar_system: "solar", user: user_1)
-# Planet.create(name: "pluton", mass: "29999999", area: "68556674745", solar_system: "solar", user: user_1)
-
 saturne_file2 = URI.open("https://res.cloudinary.com/dsas0wiz2/image/upload/v1669202835/samples/p4_ffmtkz.jpg")
 saturne_file = URI.open("https://res.cloudinary.com/dsas0wiz2/image/upload/v1669202811/samples/Mars_Girl_etkjzj.jpg")
 
